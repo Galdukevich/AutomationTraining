7ac54b7148f32852b88f203348dd3fe77f59a60c
Alexander Galdukevich <AlexanderGaldukevich@coherentsolutions.com>
2021-03-03 09:06:40 -0800

    Task20

---
+++ AutomationTrainingTasks/AutomationTrainingTasks/Pages/MainPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Task20
+{
+    public class MainPage
+    {
+        private const string _userNamePanelByClass = "uname";
+
+        private IWebDriver _driver;
+        IWebElement _UserNamePanel;
+
+        public MainPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public string GetUserName(ref string username)
+        {
+            _UserNamePanel = _driver.FindElement(By.ClassName(_userNamePanelByClass));
+            username = _UserNamePanel.Text;
+            return username;
+        }
+    }
+}
